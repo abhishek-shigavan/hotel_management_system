a877a5d9b9f174f99d1203e22cc590a9560d3f56
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-10-01 01:30:48 +0530

    Add: Defining Order with its properties

---
+++ src/utility/OrderDetails.js
@@ -0,0 +1,47 @@
+class OrderDetails {
+    #itemName;
+    #itemQuantity;
+    #itemPrice;
+    #totalAmount;
+
+    constructor(itemName, itemQuantity, itemPrice, totalAmount) {
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+        this.itemPrice = itemPrice;
+        this.totalAmount = totalAmount;
+    }
+
+    get getItemName() {
+        return this.itemName;
+    }
+
+    set setItemName(value) {
+        this.itemName = value;
+    }
+
+    get getItemQuantity() {
+        return this.itemQuantity;
+    }
+
+    set setItemQuantity(value) {
+        this.itemQuantity = value;
+    }
+
+    get getItemPrice() {
+        return this.itemPrice;
+    }
+
+    set setItemPrice(value) {
+        this.itemPrice = value;
+    }
+
+    get getTotalAmount() {
+        return this.totalAmount;
+    }
+
+    set setTotalAmount(value) {
+        this.totalAmount = value;
+    }
+}
+
+exports.OrderDetails = OrderDetails; 
