adad8ae4c20ecb943061f09c1699ec4faf00eceb
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-10-01 01:30:15 +0530

    Add: Defining User with its properties

---
+++ src/utility/UserDetails.js
@@ -0,0 +1,57 @@
+class UserDetails {
+    #firstName;
+    #lastName;
+    #gender;
+    #mobNo;
+    #userId;
+    
+    constructor(firstName, lastName, gender, mobNo, userID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.mobNo = mobNo;
+        this.userID = userID;
+    }
+
+    get getFirstName() {
+        return this.firstName;
+    }
+
+    set setFirstName(value) {
+        this.firstName = value;
+    }
+
+    get getLastName() {
+        return this.lastName;
+    }
+
+    set setLastName(value) {
+        this.lastName = value;
+    }
+
+    get getUserGender() {
+        return this.gender;
+    }
+
+    set setUserGender(value) {
+        this.gender = value;
+    }
+
+    get getUserMobNo() {
+        return this.mobNo;
+    }
+
+    set setUserMobNo(value) {
+        this.mobNo = value;
+    }
+
+    get getUserID() {
+        return this.userID;
+    }
+
+    set setUserID(value) {
+        this.userID = value;
+    }
+}
+
+exports.UserDetails = UserDetails;
