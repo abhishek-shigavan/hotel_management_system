1ab3fc710b14b9f38a43b491895c5b612537557e
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-10-04 18:30:42 +0530

    Refactor: Validating registration fields

--- src/utility/InputOutputMethods.js
+++ src/utility/InputOutputMethods.js
@@ -1,8 +1,6 @@
 const pt = require("prompt-sync")();
 const user = require('./UserDetails');
 
-const userDetailsObj = new user.UserDetails();
-
 class InputOutputMethods {
 
     generateKeyForUser(firstName, userMobile) {
@@ -10,13 +8,45 @@ class InputOutputMethods {
         return userKey;
     }
 
+    getValidFirstName() {
+        const nameRegex = RegExp("^[a-z,A-Z]{4,}$");
+        let flag = true;
+        while (flag) {
+            const firstName = pt(`Enter First Name : `);
+            if (nameRegex.test(firstName)) {
+                flag = false;
+                return firstName;
+            } else {
+                console.log(`Name should contains only letters....atleast 4 letters\n`);
+            }
+        }
+        return null;
+    }
+
+    getValidMobNo() {
+        const mobNoRegex = RegExp("^[0-9]{10}$");
+        let flag = true;
+        while (flag) {
+            const mobNo = pt(`Enter Mobile Number : `);
+            if (mobNoRegex.test(mobNo)) {
+                flag = false;
+                return mobNo;
+            } else {
+                console.log(`Invalid Mobile Number....Number should contains 10 single digit only\n`);
+            }
+        }
+        return null;
+    }
+
     registerUser() {
+        const userDetailsObj = new user.UserDetails();
+
         console.log(`\n**** Hotel Management System ****`);
         console.log(`\n**** Registration ****\n`);
-        userDetailsObj.setFirstName = pt(`Enter First Name : `);
+        userDetailsObj.setFirstName = this.getValidFirstName();
         userDetailsObj.setLastName = pt(`Enter Last Name : `);
-        userDetailsObj.setUserGender = pt('Enter Gender : ')
-        userDetailsObj.setUserMobNo = pt('Enter Mobile Number : ');
+        userDetailsObj.setUserGender = pt('Enter Gender : ');
+        userDetailsObj.setUserMobNo = this.getValidMobNo();
         const userKey = this.generateKeyForUser(userDetailsObj.getFirstName, userDetailsObj.getUserMobNo);
         userDetailsObj.setUserID = userKey;
         console.log(`\nRegistration Is Sucessfull...!!!`);
@@ -24,11 +54,37 @@ class InputOutputMethods {
         return userDetailsObj;
     }
 
+    showLoginOptions() {
+        let flag = true;
+        let userLoginId = 0;
+        let loginChoice = 0;
+        while (flag) {
+            console.log(`\n**** Hotel Management System ****\n\n 1. Admin Login \n 2. User Login\n`);
+            loginChoice = + pt(`Enter Login Type (1/2) : `);
+            switch (loginChoice) {
+                case 1:
+                    console.log(`\n**** Admin Login ****\n`);
+                    userLoginId = pt(`Enter Your Key : `);
+                    flag = false;
+                    break;
+                case 2:
+                    console.log(`\n**** User Login ****\n`);
+                    userLoginId = pt(`Enter Your Key : `);
+                    flag = false;
+                    break;
+                default:
+                    console.log(`\nEnter 1 / 2 only`);
+                    break;
+            }
+        }
+        return userLoginId;
+    }
+
     showAdminLoginMenu() {
         let flag = true;
         console.log(`\n**** Hotel Management System ****\n`);
         console.log(`Welcome Admin To Hotel Management System\n`);
-        while(flag) {    
+        while (flag) {
             console.log('1. Print Report');
             console.log(`2. Logout`);
             const adminChoice = + pt(`Enter 1 / 2 : `);
@@ -41,32 +97,6 @@ class InputOutputMethods {
             }
         }
         return null;
-    }
-
-    showLoginOptions() {
-        let flag = true;
-        let userLoginId = 0;
-        let loginChoice = 0;
-        while (flag) {
-            console.log(`\n**** Hotel Management System ****\n\n 1. Admin Login \n 2. User Login\n`);
-            loginChoice = pt(`Enter Login Type (1/2) : `);
-            switch (loginChoice) {
-                case "1":
-                    console.log(`\n**** Admin Login ****\n`);
-                    userLoginId = pt(`Enter Your Key : `);
-                    flag = false;
-                    break;
-                case "2":
-                    console.log(`\n**** User Login ****\n`);
-                    userLoginId = pt(`Enter Your Key : `);
-                    flag = false;
-                    break;
-                default:
-                    console.log(`\nEnter 1 / 2 only`);
-                    break;
-            }
-        }
-        return userLoginId;
     }
 
     userOperationMenu() {
@@ -84,7 +114,7 @@ class InputOutputMethods {
             console.log(` 2. Breakfast    Rs.150`);
             console.log(` 3. Lunch        Rs.250`);
             console.log(` 4. Dinner       Rs.300\n`);
-            const selectedItem =+ pt(`Press 1 - 4 According To FoodItem : `);
+            const selectedItem = + pt(`Press 1 - 4 According To FoodItem : `);
             if (selectedItem > 0 && selectedItem < 5) {
                 flag = false;
                 return selectedItem;
